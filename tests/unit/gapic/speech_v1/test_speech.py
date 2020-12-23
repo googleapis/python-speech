8f0b04511bc62b6ce1d92500b1ec1d98accbd6d7
yoshi-automation <yoshi-automation@google.com>
2020-12-23 08:54:02 -0800

    fix: remove client recv msg limit fix: add enums to `types/__init__.py`
    PiperOrigin-RevId: 347055288
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Fri Dec 11 12:44:37 2020 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: dd372aa22ded7a8ba6f0e03a80e06358a3fa0907
    Source-Link: https://github.com/googleapis/googleapis/commit/dd372aa22ded7a8ba6f0e03a80e06358a3fa0907

--- tests/unit/gapic/speech_v1/test_speech.py
+++ tests/unit/gapic/speech_v1/test_speech.py
@@ -1063,6 +1063,10 @@ def test_speech_transport_channel_mtls_with_client_cert_source(transport_class):
                 scopes=("https://www.googleapis.com/auth/cloud-platform",),
                 ssl_credentials=mock_ssl_cred,
                 quota_project_id=None,
+                options=[
+                    ("grpc.max_send_message_length", -1),
+                    ("grpc.max_receive_message_length", -1),
+                ],
             )
             assert transport.grpc_channel == mock_grpc_channel
             assert transport._ssl_channel_credentials == mock_ssl_cred
@@ -1101,6 +1105,10 @@ def test_speech_transport_channel_mtls_with_adc(transport_class):
                 scopes=("https://www.googleapis.com/auth/cloud-platform",),
                 ssl_credentials=mock_ssl_cred,
                 quota_project_id=None,
+                options=[
+                    ("grpc.max_send_message_length", -1),
+                    ("grpc.max_receive_message_length", -1),
+                ],
             )
             assert transport.grpc_channel == mock_grpc_channel
 
--- tests/unit/gapic/speech_v1p1beta1/test_speech.py
+++ tests/unit/gapic/speech_v1p1beta1/test_speech.py
@@ -1064,6 +1064,10 @@ def test_speech_transport_channel_mtls_with_client_cert_source(transport_class):
                 scopes=("https://www.googleapis.com/auth/cloud-platform",),
                 ssl_credentials=mock_ssl_cred,
                 quota_project_id=None,
+                options=[
+                    ("grpc.max_send_message_length", -1),
+                    ("grpc.max_receive_message_length", -1),
+                ],
             )
             assert transport.grpc_channel == mock_grpc_channel
             assert transport._ssl_channel_credentials == mock_ssl_cred
@@ -1102,6 +1106,10 @@ def test_speech_transport_channel_mtls_with_adc(transport_class):
                 scopes=("https://www.googleapis.com/auth/cloud-platform",),
                 ssl_credentials=mock_ssl_cred,
                 quota_project_id=None,
+                options=[
+                    ("grpc.max_send_message_length", -1),
+                    ("grpc.max_receive_message_length", -1),
+                ],
             )
             assert transport.grpc_channel == mock_grpc_channel
 
