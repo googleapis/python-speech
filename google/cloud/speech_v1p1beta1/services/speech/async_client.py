4e0a0dd2d1e77710033299cafbdfad3a74e13842
yoshi-automation <yoshi-automation@google.com>
2020-12-23 08:50:16 -0800

    build(node): provide hint to npm
    PiperOrigin-RevId: 339261149
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Oct 27 09:09:14 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: c9d7721bd4e22b3a25d914248fac126a5a91d3d9
    Source-Link: https://github.com/googleapis/googleapis/commit/c9d7721bd4e22b3a25d914248fac126a5a91d3d9

--- google/cloud/speech_v1p1beta1/services/speech/async_client.py
+++ google/cloud/speech_v1p1beta1/services/speech/async_client.py
@@ -178,7 +178,7 @@ class SpeechAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
+                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
                 ),
             ),
             default_timeout=5000.0,
@@ -388,7 +388,7 @@ class SpeechAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
+                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
                 ),
             ),
             default_timeout=5000.0,
