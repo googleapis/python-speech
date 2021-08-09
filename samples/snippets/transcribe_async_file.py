4bcd326c6557287bd81da40ac1c8badc3eb9fb3c
Nick Cain <nicholascain@google.com>
2021-08-09 18:34:46 +0000

    refactor: Refactor transcribe samples into two separate samples (local-file vs gcs) and remove io dependency. Refactor tests.

--- samples/snippets/transcribe_async_file.py
+++ samples/snippets/transcribe_async_file.py
@@ -22,7 +22,6 @@ Example usage:
 """
 
 import argparse
-import io
 
 
 # [START speech_transcribe_async]
@@ -33,7 +32,7 @@ def transcribe_file(speech_file):
     client = speech.SpeechClient()
 
     # [START speech_python_migration_async_request]
-    with io.open(speech_file, "rb") as audio_file:
+    with open(speech_file, "rb") as audio_file:
         content = audio_file.read()
 
     """
@@ -68,43 +67,10 @@ def transcribe_file(speech_file):
 # [END speech_transcribe_async]
 
 
-# [START speech_transcribe_async_gcs]
-def transcribe_gcs(gcs_uri):
-    """Asynchronously transcribes the audio file specified by the gcs_uri."""
-    from google.cloud import speech
-
-    client = speech.SpeechClient()
-
-    audio = speech.RecognitionAudio(uri=gcs_uri)
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
-        sample_rate_hertz=16000,
-        language_code="en-US",
-    )
-
-    operation = client.long_running_recognize(config=config, audio=audio)
-
-    print("Waiting for operation to complete...")
-    response = operation.result(timeout=90)
-
-    # Each result is for a consecutive portion of the audio. Iterate through
-    # them to get the transcripts for the entire audio file.
-    for result in response.results:
-        # The first alternative is the most likely one for this portion.
-        print(u"Transcript: {}".format(result.alternatives[0].transcript))
-        print("Confidence: {}".format(result.alternatives[0].confidence))
-
-
-# [END speech_transcribe_async_gcs]
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
     )
-    parser.add_argument("path", help="File or GCS path for audio file to be recognized")
+    parser.add_argument("--path", help="File or GCS path for audio file to be recognized")
     args = parser.parse_args()
-    if args.path.startswith("gs://"):
-        transcribe_gcs(args.path)
-    else:
-        transcribe_file(args.path)
+    transcribe_file(args.path)
--- samples/snippets/transcribe_async_gcs_test.py
+++ samples/snippets/transcribe_async_gcs_test.py
@@ -14,20 +14,13 @@
 import os
 import re
 
-import transcribe_async
+import transcribe_async_gcs
 
 RESOURCES = os.path.join(os.path.dirname(__file__), "resources")
 
 
-def test_transcribe(capsys):
-    transcribe_async.transcribe_file(os.path.join(RESOURCES, "audio.raw"))
-    out, err = capsys.readouterr()
-
-    assert re.search(r"how old is the Brooklyn Bridge", out, re.DOTALL | re.I)
-
-
 def test_transcribe_gcs(capsys):
-    transcribe_async.transcribe_gcs("gs://python-docs-samples-tests/speech/audio.flac")
+    transcribe_async_gcs.transcribe_gcs("gs://python-docs-samples-tests/speech/audio.flac")
     out, err = capsys.readouterr()
 
     assert re.search(r"how old is the Brooklyn Bridge", out, re.DOTALL | re.I)
