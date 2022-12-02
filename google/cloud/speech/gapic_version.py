ff04d117298125f461ae6397e8ec8460c57b5a2e
Anthonios Partheniou <partheniou@google.com>
2022-12-02 19:42:24 +0000

    chore: use templated setup.py and owlbot.py

--- google/cloud/speech/gapic_version.py
+++ google/cloud/speech/gapic_version.py
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-__version__ = "0.1.0"
+__version__ = "2.16.2"  # {x-release-please-version}
